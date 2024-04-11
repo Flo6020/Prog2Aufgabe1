233a56196e176d0d5427133a9a34ae030df82069
Flo6020 <jf4659@mci4me.at>
2024-04-11 16:08:36 +0200

    corrected with minuts on xaxis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from load_data import load_data
 from sort import bubble_sort
 import matplotlib.pyplot as plt
+import numpy as np
 
 load_data('activity.csv')
 data = load_data('activity.csv')
@@ -9,8 +10,12 @@ power_W = data['PowerOriginal']
 sorted_power_W = bubble_sort(power_W)
 print(sorted_power_W[::-1])
 
-plt.plot(sorted_power_W[::-1])
-
+time = np.array(range(0, len(sorted_power_W)))
+time = time/60
+plt.plot(time, sorted_power_W[::-1])
+plt.xlabel('Time')
+plt.ylabel('Power')
+plt.grid(True)
 
 #save in figures
 plt.savefig('figures/plot.png')
